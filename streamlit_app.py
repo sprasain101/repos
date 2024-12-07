72daaaf232cbd4be5d361b4cd42e58ffa796efb3
sprasain101 <sandesh.prasain5@gmail.com>
2024-12-07 18:58:18 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import re
 
-st.write("Hello World")
-st.write("Favorite movie?")
+st.title("Web Scraper")
+pattern = "[a-zA-Z0-9]+@[a-zA-Z]+\.(com|edu|net)"
+x = st.text_input("Enter the url for scrapping: ")
+st.write(f"URL: {x}")
+# if re.match(pattern, x):
+#    st.write(f"URL: {x}")
+# else:
+#    st.write(f"invalid url")
