36351f4cbd2a37366f6ffffb7ca6c42afadb446e
sprasain101 <sandesh.prasain5@gmail.com>
2024-12-08 09:53:18 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,15 @@
 import streamlit as st
 import re
 
+st.image("world.jpg")
 st.title("Web Scraper")
 pattern = "[a-zA-Z0-9]+@[a-zA-Z]+\.(com|edu|net)"
 x = st.text_input("Enter the url for scrapping: ")
 st.write(f"URL: {x}")
-# if re.match(pattern, x):
+# if re.search(pattern, x):
 #    st.write(f"URL: {x}")
 # else:
 #    st.write(f"invalid url")
+
+st.selectbox("Choose the scraper to scrape the website", ["BeautifulSoup", "Selenium"])
+st.radio("Choose the file for the data to be stored", ["CSV", "JSON"])
