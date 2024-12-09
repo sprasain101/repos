138ba28e2ce8219297414f5844e7d9f608c8371c
sprasain101 <sandesh.prasain5@gmail.com>
2024-12-09 09:55:24 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,25 @@
 import streamlit as st
 import re
 
-st.image("world.jpg")
-st.title("Web Scraper")
-pattern = "[a-zA-Z0-9]+@[a-zA-Z]+\.(com|edu|net)"
-x = st.text_input("Enter the url for scrapping: ")
-st.write(f"URL: {x}")
-# if re.search(pattern, x):
-#    st.write(f"URL: {x}")
-# else:
-#    st.write(f"invalid url")
+class MainWindow():
+    def __init__(self):
+        self.__init__
 
-st.selectbox("Choose the scraper to scrape the website", ["BeautifulSoup", "Selenium"])
-st.radio("Choose the file for the data to be stored", ["CSV", "JSON"])
+    def run():
+        st.image("world.jpg")
+        st.title("Web Scraper")
+        pattern = "[a-zA-Z0-9]+@[a-zA-Z]+\.(com|edu|net)"
+        x = st.text_input("Enter the url for scrapping: ")
+        st.write(f"URL: {x}")
+        # if re.search(pattern, x):
+        #    st.write(f"URL: {x}")
+        # else:
+        #    st.write(f"invalid url")
+
+        st.selectbox("Choose the scraper to scrape the website", ["BeautifulSoup", "Selenium"])
+        st.radio("Choose the file for the data to be stored", ["CSV", "JSON"])
+        btn = st.button("Click")
+        if btn:
+            st.write("button clicked")
+
+MainWindow.run()
