57d09edb946c3afb21009a969daf63c6112f4a1c
sprasain101 <sandesh.prasain5@gmail.com>
2025-02-15 11:38:46 +0545

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Multiplication{
+    public static void main(String[] args){
+        final int NUM1 = 2000000;
+        final int NUM2 = 4000000;
+        int multiply = NUM1 * NUM2;
+        System.out.println("Result="+multiply);
+    }
+}
